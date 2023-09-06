db6556fed88b5b68950855d95380a84ac6f830f0
NovaBro <57100555+NovaBro@users.noreply.github.com>
2023-09-05 22:11:59 -0400

    working

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ class monster():
     def __init__(self, index) -> None:
         self.monsterIdentity = index
         self.speed = 0.01
-        self.hitRadius = 0.25
-        self.searchRadius = 1
+        self.hitRadius = 1
+        self.searchRadius = 2
         self.directionHeading = 0
+
         
     def goToPoint(self, point:list, globalPositions:np.ndarray):
         xDiff = point[0] - globalPositions[self.monsterIdentity][0]
@@ -23,26 +24,57 @@ class monster():
         newYposition = globalPositions[self.monsterIdentity][1] + self.speed * yDiff/distance
         if distance > self.hitRadius and -5 < newXposition < 5 and -5 < newYposition < 5:
             globalPositions[self.monsterIdentity] = np.array([newXposition, newYposition])
-            self.directionHeading = np.arctan(yDiff/xDiff)
 
-    def searchArea(self, globalPositions:np.ndarray):
-        totalDirection = 0
-        detected = 0
-        for monster in globalPositions:
-            xDiff = monster[0] - globalPositions[self.monsterIdentity][0]
-            yDiff = monster[1] - globalPositions[self.monsterIdentity][1]
-            distance = np.sqrt(xDiff ** 2 + yDiff ** 2)
-            if distance < self.searchArea:
-                detected += 1
-                totalDirection += np.arctan(yDiff/xDiff)
+
+    def distanceCalculation(self, x, y):
+        return np.sqrt(x ** 2 + y ** 2)
+    
+    def calculatePoint(self, populationDirection, avgHeading, influenceFactor):
+        goDirection = (populationDirection + avgHeading) * influenceFactor + self.directionHeading
+        return [np.cos(goDirection), np.sin(goDirection)]
         
-        self.directionHeading = -(totalDirection/detected)
         
 
 
-    def update(self, point:list, globalPositions:np.ndarray):
-        self.goToPoint(point, globalPositions)
+    def searchArea(self, globalPositions:np.ndarray, allObjects:list):
+        totalXDiff = 0
+        totalYDiff = 0
+        searchCount = 0
+        avgXDis = 0
+        avgYDis = 0
+        avgHeading = 0
 
+
+        for i in range(len(globalPositions)):
+            xDiff = globalPositions[i][0] - globalPositions[self.monsterIdentity][0]
+            yDiff = globalPositions[i][1] - globalPositions[self.monsterIdentity][1]
+
+            distance = self.distanceCalculation(xDiff, yDiff)
+            if distance < self.searchRadius:
+                totalXDiff += xDiff
+                totalYDiff += yDiff
+                searchCount += 1
+                avgHeading += allObjects[i].directionHeading
+        
+        if searchCount != 0:
+            avgXDis = totalXDiff / searchCount
+            avgYDis = totalYDiff / searchCount
+            avgHeading /= searchCount
+
+        if self.distanceCalculation(avgXDis, avgYDis) < self.hitRadius:
+            populationDirection = np.arctan(avgYDis/avgXDis) + np.pi
+            self.goToPoint(self.calculatePoint
+                           (populationDirection, avgHeading, 0.2), globalPositions)
+
+        else:
+            populationDirection = np.arctan(avgYDis/avgXDis)
+            self.goToPoint(self.calculatePoint
+                           (populationDirection, avgHeading, 0.2), globalPositions)
+
+    def update(self, globalPositions:np.ndarray, allObjects:list):
+        self.searchArea(globalPositions, allObjects)
+
+        #self.goToPoint(point, globalPositions)
 
 class food():
     def __init__(self) -> None:
@@ -70,9 +102,10 @@ def plotObjects(allObjects:list):
     return allArtists
 
 def animateFunction(i):
-    print(i)
+    #print(i)
     for o in allObjects:
-        o.update([1,1], globalPositions)
+        o.update(globalPositions, allObjects)
+        print(globalPositions)
 
     return plotObjects(allObjects)
 
