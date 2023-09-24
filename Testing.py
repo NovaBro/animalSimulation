3bd9c0bdcc3a6dc7643944a561622fc275aa6dce
NovaBro <57100555+NovaBro@users.noreply.github.com>
2023-09-24 13:17:58 -0400

    update

--- Testing.py
+++ Testing.py
@@ -5,4 +5,20 @@ import pandas as pd
 
 fig = plt.figure()
 ax = fig.add_subplot()
-ax.scatter()
+
+
+print(np.array([[0,0],[1,1],[2,2]]).shape)
+a = np.array([[1, 5],
+              [2, 1]])
+b = np.array([[4, 1],
+              [2, 2]])
+#print(np.matmul(a,b), np.matmul(b,a))
+c = np.array([4,1,2,3])
+d = np.array([5,1,2])
+
+e = c[:,np.newaxis] + d[:]
+print(e)
+
+print(np.linalg.norm(e, axis = 1))
+print(np.matmul(d[:,np.newaxis],c[np.newaxis,:]))
+
